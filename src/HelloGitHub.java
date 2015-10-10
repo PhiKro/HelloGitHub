118cb7811f023c06580216c69ada80fc5b91450c
Philipp <philipp.kropik@gmail.com>
2015-10-10 14:03:48 +0200

    test

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -1,11 +1,10 @@
-
 public class HelloGitHub {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello GitHub!");
 		System.out.println("Das ist ein Test");
 	}
-
+	
 	
 }
